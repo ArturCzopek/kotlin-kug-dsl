a6c8fe8469ffa66fb814e5c746db3f6b26d6bed8
Artur <arturcz32@gmail.com>
2019-05-12 10:20:35 +0200

    5. DSL - FIRST PART, TOURNAMENT + BATTLES

--- src/pl/simplecoding/kug/dsl/Main.kt
+++ src/pl/simplecoding/kug/dsl/Main.kt
@@ -1,35 +1,36 @@
 package pl.simplecoding.kug.dsl
 
-fun main(args: Array<String>) {
-  val tournament = Tournament(
-      "KUG Tournament",
-      battles(
-          Hero(
-              name = "John",
-              profession = Profession.KNIGHT,
-              level = 20,
-              equipment = items(
-                  "Sword" power 20,
-                  "Shield" power 5,
-                  "Nike Air Max" power 40
-              ),
-              spells = spells(
-                  "Hohoho" spellPower 4
-              )
-          ) versus Hero(
-              name = "Mike",
-              profession = Profession.WIZARD,
-              level = 25,
-              equipment = items(
-                  "Rod" power 15
-              ),
-              spells = spells(
-                  "Vingardium Leviosa" spellPower 20,
-                  "Not Found" spellPower 40
-              )
-          )
-      )
-  )
+  fun main(args: Array<String>) {
+    val tournament = tournament {
+      name = "KUG Tournament"
+      battles {
+        Hero(
+            name = "John",
+            profession = Profession.KNIGHT,
+            level = 20,
+            equipment = items(
+                "Sword" power 20,
+                "Shield" power 5,
+                "Nike Air Max" power 40
+            ),
+            spells = spells(
+                "Hohoho" spellPower 4
+            )
+        ) versus Hero(
+            name = "Mike",
+            profession = Profession.WIZARD,
+            level = 25,
+            equipment = items(
+                "Rod" power 15
+            ),
+            spells = spells(
+                "Vingardium Leviosa" spellPower 20,
+                "Not Found" spellPower 40
+            )
+        )
+      }
+    }
 
-  println("Tournament winner: ${tournament.winner?.name ?: "NO ONE"}")
+
+    println("Tournament winner: ${tournament.winner?.name ?: "NO ONE"}")
 }
