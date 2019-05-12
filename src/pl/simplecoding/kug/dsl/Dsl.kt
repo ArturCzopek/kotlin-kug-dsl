2cba6ea86a319096ea9ba6d867e9541c0ff8b7d6
Artur <arturcz32@gmail.com>
2019-05-12 10:22:01 +0200

    6. DSL - FINISHED DSL, APPLIED TO WHOLE TOURNAMENT

--- src/pl/simplecoding/kug/dsl/Dsl.kt
+++ src/pl/simplecoding/kug/dsl/Dsl.kt
@@ -2,6 +2,10 @@ package pl.simplecoding.kug.dsl
 
 fun tournament(block: TournamentBuilder.() -> Unit): Tournament = TournamentBuilder().apply(block).build()
 
+fun wizard(name: String, block: HeroBuilder.() -> Unit) = HeroBuilder(name, Profession.WIZARD).apply(block).build()
+
+fun knight(name: String, block: HeroBuilder.() -> Unit) = HeroBuilder(name, Profession.KNIGHT).apply(block).build()
+
 class TournamentBuilder {
   var name: String = ""
 
@@ -16,4 +20,28 @@ class TournamentBuilder {
 
 class Battles: ArrayList<Battle>() {
   infix fun Hero.versus(opponent: Hero) = add(Battle(this, opponent))
+}
+
+class HeroBuilder(private val name: String, private val profession: Profession) {
+  var level = 0
+  var equipment = mutableListOf<Item>()
+  var spells = mutableListOf<Spell>()
+
+  fun equipment(block: Items.() -> Unit) {
+    equipment.addAll(Items().apply(block))
+  }
+
+  fun spells(block: Spells.() -> Unit) {
+    spells.addAll(Spells().apply(block))
+  }
+
+  fun build() = Hero(name, profession, level, equipment, spells)
+}
+
+class Items: ArrayList<Item>() {
+  infix fun String.power(power: Int) = add(Item(this, power))
+}
+
+class Spells: ArrayList<Spell>() {
+  infix fun String.power(power: Int) = add(Spell(this, power))
 }
--- src/pl/simplecoding/kug/dsl/Main.kt
+++ src/pl/simplecoding/kug/dsl/Main.kt
@@ -4,33 +4,28 @@ package pl.simplecoding.kug.dsl
     val tournament = tournament {
       name = "KUG Tournament"
       battles {
-        Hero(
-            name = "John",
-            profession = Profession.KNIGHT,
-            level = 20,
-            equipment = items(
-                "Sword" power 20,
-                "Shield" power 5,
-                "Nike Air Max" power 40
-            ),
-            spells = spells(
-                "Hohoho" spellPower 4
-            )
-        ) versus Hero(
-            name = "Mike",
-            profession = Profession.WIZARD,
-            level = 25,
-            equipment = items(
-                "Rod" power 15
-            ),
-            spells = spells(
-                "Vingardium Leviosa" spellPower 20,
-                "Not Found" spellPower 40
-            )
-        )
+        knight("John") {
+          level = 20
+          equipment {
+            "Sword" power 20
+            "Shield" power 5
+            "Nike Air Max" power 40
+          }
+          spells {
+            "Hohoho" spellPower 4
+          }
+        } versus wizard("Mike") {
+          level = 25
+          equipment {
+            "Rod" power 15
+          }
+          spells {
+            "Vingardium Leviosa" spellPower 20
+            "Not Found" spellPower 40
+          }
+        }
       }
     }
-
 
     println("Tournament winner: ${tournament.winner?.name ?: "NO ONE"}")
 }
